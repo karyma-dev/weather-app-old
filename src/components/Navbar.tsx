41af0f2b57fcde280f36a05b683f5aa7ce962340
Kary Ma <karyma101@hotmail.com>
2020-06-09 10:09:43 -0700

    added tsconfig and convert js files to tsx files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import { WeatherContext } from '../contexts/WeatherContext/index.tsx'
+
+const Navbar = () => {
+  const { weather, weatherDispatch } = useContext(WeatherContext)
+
+  const backButton = weather ? (
+    <button
+      className="header-button"
+      onClick={() => weatherDispatch({ type: 'RESET' })}
+    >
+      Back
+    </button>
+  ) : null
+
+  return <header className="header">{backButton}</header>
+}
+
+export default Navbar
---
+++ src/components/TodayForecast.tsx
@@ -0,0 +1,20 @@
+import React, { Fragment, useContext } from 'react'
+import { WeatherContext } from '../contexts/WeatherContext/index.tsx'
+
+const TodayForecast = () => {
+  const { description, temperature, humidity, iconId } = useContext(
+    WeatherContext
+  )
+
+  return (
+    <Fragment>
+      <div className="today-description">{description}</div>
+      <div className="today-group">
+        <img src={`http://openweathermap.org/img/wn/${iconId}.png`} />
+        <span className="today-group_temp">{temperature}</span>
+      </div>
+    </Fragment>
+  )
+}
+
+export default TodayForecast
